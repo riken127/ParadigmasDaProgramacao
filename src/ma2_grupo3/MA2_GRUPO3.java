b8796100041943f23a4957cd95b77fc8a4d019be
riken <hnoronha401@gmail.com>
2023-05-26 20:28:47 +0100

    mudança MA2_GRUPO3

--- src/ma2_grupo3/MA2_GRUPO3.java
+++ src/ma2_grupo3/MA2_GRUPO3.java
@@ -14,9 +14,8 @@ public class MA2_GRUPO3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        
+        // TODO code application logic here   
+        System.out.println("Hello World!");
     }
-    
+
 }
