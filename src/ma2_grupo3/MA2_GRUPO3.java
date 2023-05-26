8c2682aa3f0ca5fa5b6e04528d35a177a18d64f2
riken <hnoronha401@gmail.com>
2023-05-26 20:37:35 +0100

    testezão no netbeans

--- src/ma2_grupo3/MA2_GRUPO3.java
+++ src/ma2_grupo3/MA2_GRUPO3.java
@@ -15,7 +15,7 @@ public class MA2_GRUPO3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here   
-        System.out.println("Hello World!");
+        System.out.println("Vamos Zé!");
     }
 
 }
