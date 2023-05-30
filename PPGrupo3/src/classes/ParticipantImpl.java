3cf693dccbe05b7b166da2f585e905993d79da1d
NatanFC22 <joaopereira237@gmail.com>
2023-05-30 10:23:36 +0100

    Package Classes

---
+++ PPGrupo3/src/classes/ParticipantImpl.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import cbl.participants.Contact;
+import cbl.participants.Instituition;
+import cbl.participants.Participant;
+
+/**
+ *
+ * @author User
+ */
+public class ParticipantImpl implements Participant{
+
+    private String name;
+    private String email;
+    private Instituition instituition;
+    private Contact contact;
+
+    public ParticipantImpl(String name, String email, Instituition instituition, Contact contact) {
+        this.name = name;
+        this.email = email;
+        this.instituition = instituition;
+        this.contact = contact;
+    }
+    
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getEmail() {
+        return this.email;
+    }
+
+    @Override
+    public Contact getContact() {
+        return this.contact;
+    }
+
+    @Override
+    public Instituition getInstituition() {
+        return this.instituition;
+    }
+
+    @Override
+    public void setInstituition(Instituition instn) {
+        this.instituition = instn;
+    }
+
+    @Override
+    public void setContact(Contact cntct) {
+        this.contact = cntct;
+    }
+    
+}
---
+++ PPGrupo3/src/classes/StudentImpl.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import cbl.participants.Contact;
+import cbl.participants.Instituition;
+import cbl.participants.Student;
+
+/**
+ *
+ * @author User
+ */
+public class StudentImpl extends ParticipantImpl implements Student {
+
+    private int number;
+    
+    public StudentImpl(String name, String email, Instituition instituition, Contact contact, int number) {
+        super(name, email, instituition, contact);
+        this.number = number;
+    }
+
+    @Override
+    public int getNumber() {
+        return this.number;
+    }
+    
+}
