f1d0e3a0b75000fdcfadcdc614dc72980169a8a4
riken127 <hnoronha401@gmail.com>
2023-06-01 14:52:55 +0100

    commit

--- PPGrupo3/src/project/TaskImpl.java
+++ PPGrupo3/src/project/TaskImpl.java
@@ -9,6 +9,7 @@
 */
 package project;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import ma02_resources.project.Submission;
 import ma02_resources.project.Task;
@@ -18,16 +19,14 @@ public class TaskImpl implements Task {
     private static final int INITIAL_SUBMISSION_SIZE = 20;
     private LocalDate start;
     private LocalDate end;
-    private int duration;
     private String title;
     private String description;
     private Submission[] submissions;
     private int submissionCounter;
 
-    public TaskImpl(LocalDate start, LocalDate end, int duration, String title, String description) {
+    public TaskImpl(LocalDate start, LocalDate end, String title, String description) {
         this.start = start;
         this.end = end;
-        this.duration = duration;
         this.title = title;
         this.description = description;
         this.submissions = new Submission[INITIAL_SUBMISSION_SIZE];
@@ -46,7 +45,7 @@ public class TaskImpl implements Task {
 
     @Override
     public int getDuration() {
-        return duration;
+        return ((int) Duration.between(this.getStart(), this.getEnd()).toDays());
     }
 
     @Override
@@ -99,4 +98,18 @@ public class TaskImpl implements Task {
         return (this.getStart().compareTo(task.getStart()));
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)  {
+            return false;
+        }
+        if (!(obj instanceof Task)) {
+            return false;
+        }
+        TaskImpl temporaryTask = (TaskImpl) obj;
+        return (temporaryTask.getTitle().equals(this.getTitle()) &&
+            temporaryTask.getDescription().equals(this.getDescription()) &&
+            temporaryTask.getStart().equals(this.getStart()) &&
+            temporaryTask.getEnd().equals(this.getEnd()));
+    }
 }
